ac1d85c8a2b86d88c63666db1208fd930bd196b5
Craig Cook <cncook001@yahoo.com>
2021-11-07 20:47:09 -0500

    trying again

--- make_index.py
+++ make_index.py
@@ -1,9 +1,8 @@
 """ Build index from directory listing
-
 make_index.py --path </path/to/directory> [--directory <directory>]
 """
 from __future__ import print_function
-#from mako.template import Template
+from mako.template import Template
 import argparse
 import os
 import os.path
@@ -16,7 +15,6 @@ INDEX_TEMPLATE = r"""
 <meta name="description" content="${directory}"/>
 </head>
 <body>
-
     <h2>Index of ${directory}</h2>
     <p>
     <table>
@@ -43,7 +41,6 @@ INDEX_TEMPLATE = r"""
             <tr>
                 <th colspan="5"><hr></th>
             </tr>
-
             % for name in dirnames:
             <tr>
                 <td valign="top"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAd5JREFUeNqMU79rFUEQ/vbuodFEEkzAImBpkUabFP4ldpaJhZXYm/RiZWsv/hkWFglBUyTIgyAIIfgIRjHv3r39MePM7N3LcbxAFvZ2b2bn22/mm3XMjF+HL3YW7q28YSIw8mBKoBihhhgCsoORot9d3/ywg3YowMXwNde/PzGnk2vn6PitrT+/PGeNaecg4+qNY3D43vy16A5wDDd4Aqg/ngmrjl/GoN0U5V1QquHQG3q+TPDVhVwyBffcmQGJmSVfyZk7R3SngI4JKfwDJ2+05zIg8gbiereTZRHhJ5KCMOwDFLjhoBTn2g0ghagfKeIYJDPFyibJVBtTREwq60SpYvh5++PpwatHsxSm9QRLSQpEVSd7/TYJUb49TX7gztpjjEffnoVw66+Ytovs14Yp7HaKmUXeX9rKUoMoLNW3srqI5fWn8JejrVkK0QcrkFLOgS39yoKUQe292WJ1guUHG8K2o8K00oO1BTvXoW4yasclUTgZYJY9aFNfAThX5CZRmczAV52oAPoupHhWRIUUAOoyUIlYVaAa/VbLbyiZUiyFbjQFNwiZQSGl4IDy9sO5Wrty0QLKhdZPxmgGcDo8ejn+c/6eiK9poz15Kw7Dr/vN/z6W7q++091/AQYA5mZ8GYJ9K0AAAAAASUVORK5CYII= "
@@ -54,7 +51,6 @@ INDEX_TEMPLATE = r"""
                 <td>&nbsp;</td>
             </tr>
             % endfor
-
             % for name in filenames:
             <tr>
                 <td valign="top"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAABnRSTlMAAAAAAABupgeRAAABHUlEQVR42o2RMW7DIBiF3498iHRJD5JKHurL+CRVBp+i2T16tTynF2gO0KSb5ZrBBl4HHDBuK/WXACH4eO9/CAAAbdvijzLGNE1TVZXfZuHg6XCAQESAZXbOKaXO57eiKG6ft9PrKQIkCQqFoIiQFBGlFIB5nvM8t9aOX2Nd18oDzjnPgCDpn/BH4zh2XZdlWVmWiUK4IgCBoFMUz9eP6zRN75cLgEQhcmTQIbl72O0f9865qLAAsURAAgKBJKEtgLXWvyjLuFsThCSstb8rBCaAQhDYWgIZ7myM+TUBjDHrHlZcbMYYk34cN0YSLcgS+wL0fe9TXDMbY33fR2AYBvyQ8L0Gk8MwREBrTfKe4TpTzwhArXWi8HI84h/1DfwI5mhxJamFAAAAAElFTkSuQmCC "
@@ -65,7 +61,6 @@ INDEX_TEMPLATE = r"""
                 <td>&nbsp;</td>
             </tr>
             % endfor
-
             </p>
         </tbody>
     </table>
@@ -81,36 +76,24 @@ EXCLUDED = ['index.html']
 def fun(path, rootdir, directory):
 
     print('Processing: '+path)
-    # filenames = [fname for fname in sorted(os.listdir(path))
-    #              if fname not in EXCLUDED and os.path.isfile(path+fname)]
-    # dirnames = [fname for fname in sorted(os.listdir(path))
-    #             if fname not in EXCLUDED]
-    # dirnames = [fname for fname in dirnames if fname not in filenames]
-
-    # for path, dirnames, files in os.walk(.):
-    #     print(f'Found directory: {dirpath}')
-    # for file_name in files:
-    #     print(file_name)
-
-    for root, dirs, files in os.walk(path, topdown=False):
-        for name in files:
-            print(os.path.join(root, name))
-        for name in dirs:
-            print('Found directory:')
-            print(os.path.join(root, name))
+    filenames = [fname for fname in sorted(os.listdir(path))
+                 if fname not in EXCLUDED and os.path.isfile(path+fname)]
+    dirnames = [fname for fname in sorted(os.listdir(path))
+                if fname not in EXCLUDED]
+    dirnames = [fname for fname in dirnames if fname not in filenames]
 
     if not directory:
         directory = os.path.basename(path)
 
-    # f = open(path+'/index.html', 'w')
-    # print(Template(INDEX_TEMPLATE).render(
-    #  dirnames=dirnames,
-    #  filenames=filenames,
-    #  directory=directory,
-    #  ROOTDIR=rootdir,
-    #  time=time.ctime(os.path.getctime(path))
-    #  ), file=f)
-    # f.close()
+    f = open(path+'/index.html', 'w')
+    print(Template(INDEX_TEMPLATE).render(
+     dirnames=dirnames,
+     filenames=filenames,
+     directory=directory,
+     ROOTDIR=rootdir,
+     time=time.ctime(os.path.getctime(path))
+     ), file=f)
+    f.close()
 
     for subdir in dirnames:
         try:
